2e91dbcda893ae0f09f797de5bc096ff68ba793f
matteobonelli <matteobonelli08@gmail.com>
2023-10-10 14:53:08 +0200

    added price calculator js

--- js/script.js
+++ js/script.js
@@ -8,9 +8,22 @@ generate.addEventListener('click' , function(){
     const nameSurname = document.getElementById('nome').value;
     const km = parseInt(document.getElementById('km').value);
     const age = document.getElementById('age').value;
+    let price = kmPrice * km;
+    let priceUnder18 = price * under18Discount;
+    let priceOver65 = price * over65Discount;
     console.log(nameSurname, km, age);
+    if(age === 'under18'){
+        price = price - priceUnder18;
+    } else if(age === 'over65'){
+        price = price - priceOver65;
+    }
+    console.log(price);
     if(nameSurname === '' || km === '' || isNaN(km)){
         const wrongText = document.getElementById('wrong-text');
         wrongText.classList.remove('d-none')
+    } else{
+        const ticket = document.getElementById('ticket');
+        ticket.classList.remove('d-none');
+
     }
 })
